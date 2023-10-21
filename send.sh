e08a8cd0dbf782b7054a9c5d04a5d1e342f6eb83
rtldg <rtldg@protonmail.com>
2023-10-21 10:29:55 +0000

    Script fixups

--- send.sh
+++ send.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -trvz --delete web/* admin@ec2:/web
+rsync -trvz --delete --exclude cookiepy/venv --exclude c.ookie.click/er/topeka web/* admin@ec2:/web
--- web/cookiepy/setup-venv.sh
+++ web/cookiepy/setup-venv.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euxo pipefail
 python3 -m venv venv
 source venv/bin/activate
 pip install gunicorn flask
