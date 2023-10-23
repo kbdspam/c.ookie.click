ed7876b469a805e45c24c83e1a8fb55d78c30f85
rtldg <rtldg@protonmail.com>
2023-10-23 00:32:54 +0000

    Almost done!

--- ookieLeaderboard/main.js
+++ ookieLeaderboard/main.js
@@ -14,6 +14,8 @@ Game.registerMod("ookieLeaderboard",{
 		setTimeout(()=>document.ookieLeaderboard.leaderboard_updateme(),2*1000);//bleh
 		this.leaderboard_query();
 	},
+
+
 	leaderboard_updateme: function() {
 		if (this.updatemeInterval) clearInterval(this.updatemeInterval);
 		this.updatemeInterval = setInterval(()=>document.ookieLeaderboard.leaderboard_updateme(), this.updateS*1000); // dumb, I know
@@ -25,6 +27,22 @@ Game.registerMod("ookieLeaderboard",{
 				"X-My-Update-Data": Game.cookiesEarned+'|'+Game.cookiesPsRaw,
 			},
 		}).then(response => {});
+	},
+	leaderboard_leave: function(id) {
+		if (this.settings.cookie == "none") return;
+		const MOD = this; // I have no idea how fetch stuff affects `this`... I don't care to find out either. MOD it is...
+		fetch(this.baseURL+"/leaderboard/leave", {
+			method: "POST",
+			headers: {
+				"X-My-Cookie": this.settings.cookie,
+				"X-My-Leaderboard-ID": id.toString(),
+			},
+		}).then(response => {
+			if (response.ok) MOD.leaderboard_query();
+			else throw Error(response.statusText);
+		}).catch(err => {
+			// TODO more stuff here...
+		});
 	},
 	leaderboard_join: function(cookie) {
 		if (this.settings.cookie == "none") return;
@@ -43,10 +61,8 @@ Game.registerMod("ookieLeaderboard",{
 		});
 	},
 	leaderboard_create: function(name) {
-		console.log("hello");
 		if (this.settings.cookie == "none") return;
 		const MOD = this; // I have no idea how fetch stuff affects `this`... I don't care to find out either. MOD it is...
-		console.log("hello2");
 		fetch(this.baseURL+"/leaderboard/create", {
 			method: "POST",
 			headers: {
@@ -54,12 +70,10 @@ Game.registerMod("ookieLeaderboard",{
 				"X-My-New-Leaderboard-Name": name,
 			},
 		}).then(response => {
-			console.log("hello3");
 			MOD.waitingForRegister = false;
 			if (response.ok) return response.text();
 			else throw Error(response.statusText);
 		}).then(cookie => {
-			console.log("hello4");
 			MOD.leaderboard_query();
 		}).catch(err => {
 			// TODO more stuff here...
@@ -85,39 +99,6 @@ Game.registerMod("ookieLeaderboard",{
 			// TODO more stuff here...
 		});
 	},
-	_leaderboardTabClick: function(e) {
-		const item = e.target.id.split("leaderboardTab")[1]; // this is so stupid but it's so easy
-		if (this.tabOpenTo == item || this.tabOpenTo == +item) {
-			this.tabOpenTo = null;
-			PlaySound('snd/clickOff2.mp3');
-			if (l("leaderboardTabPage")) l("leaderboardTabPage").remove();
-			return;
-		}
-		PlaySound('snd/clickOn2.mp3');
-		if (item != "New") this.viewLeaderboardPage(+item);
-		else {
-			Game.Prompt('<id LeaderboardJoinOrCreateXXXX><h3>Join or Create a Leaderboard</h3><div class="block" style="text-align:center;">insert a name or a leaderboard-cookie</div><div class="block"><input type="text" style="text-align:center;width:100%;" id="leaderboardJoinOrCreate" value=""/></div>', [
-				["join", `
-					const s = l('leaderboardJoinOrCreate').value;
-					if (s.length == 32 && s.match(/^[0-9a-zA-Z]+$/)) {
-						ookieLeaderboard.leaderboard_join(s);
-						Game.ClosePrompt();
-					}
-				`],
-				["create", `
-					const s = l('leaderboardJoinOrCreate').value;
-					const x = (new TextEncoder().encode(s)).length;
-					if (x >= 1 && x <= 32) {
-						ookieLeaderboard.leaderboard_create(s);
-						Game.ClosePrompt();
-					}
-				`],
-				loc("Cancel"),
-			]);
-			l('leaderboardJoinOrCreate').focus();
-			l('leaderboardJoinOrCreate').select();
-		}
-	},
 	leaderboard_query: function() {
 		if (this.queryInterval) clearInterval(this.queryInterval);
 		this.queryInterval = setInterval(()=>document.ookieLeaderboard.leaderboard_query(), this.queryS*1000); // dumb, I know
@@ -140,8 +121,11 @@ Game.registerMod("ookieLeaderboard",{
 				boardvalues[b[0]] = [];
 				newTabBar += `<div class="leaderboardTab subButton" id="leaderboardTab${b[0]}">${this.escapeHTML(b[1])}</div>`;
 			}
-			if (json.boardinfo.length < 5)
-				newTabBar += '<div class="leaderboardTab subButton" id="leaderboardTabNew">join/create a new board</div>';
+			if (newTabBar == "") {
+				newTabBar = `
+					<div style="padding: 5px 5px 5px 5px;">try joining or creating a leaderboard with the button on the bottom right</div>
+				`;
+			}
 			l("leaderboardTabBar").innerHTML = newTabBar;
 			document.querySelectorAll(".leaderboardTab").forEach((tab)=>AddEvent(tab,'click',(e)=>MOD._leaderboardTabClick(e)));
 			// parse json.boardvalues to fill out MOD.boardvalues[]
@@ -153,22 +137,81 @@ Game.registerMod("ookieLeaderboard",{
 			}
 			this.boardvalues = boardvalues;
 			this.boardinfo = json.boardinfo;
+			if (this.tabOpenTo != null) this.viewLeaderboardPage(+this.tabOpenTo);
 			//if (this.dev && this.boardinfo.length > 0) setTimeout((b)=>MOD.viewLeaderboardPage(b), 0, this.boardinfo[0][0]); // TODO: TEMPORARY
 		}).catch(()=>{}); // no need to console.log because the console already spams failed network requests
 	},
-	addTabBar: function() {
-		if (l("leaderboardTabBar")) l("leaderboardTabBar").remove();
-		l('leaderboardTitle').insertAdjacentHTML('afterend', `
-			<div id="leaderboardTabBar">
-				loading...
-			</div>
-		`);
+
+
+	joinCreatePrompt: function() {
+		if (this.boardinfo.length >= 5) {
+			Game.Notify("You can't join/create any more leaderboards!",'',0,5);
+			PlaySound('snd/clickOff2.mp3');
+			return;
+		}
+		PlaySound('snd/clickOn2.mp3');
+		Game.Prompt('<id LeaderboardJoinOrCreateXXXX><h3>Join or Create a Leaderboard</h3><div class="block" style="text-align:center;">insert a name or a leaderboard-cookie</div><div class="block"><input type="text" style="text-align:center;width:100%;" id="leaderboardJoinOrCreate" value=""/></div>', [
+			["join", `
+				const s = l('leaderboardJoinOrCreate').value;
+				if (s.length == 32 && s.match(/^[0-9a-zA-Z]+$/)) {
+					ookieLeaderboard.leaderboard_join(s);
+					Game.ClosePrompt();
+				}
+			`],
+			["create", `
+				const s = l('leaderboardJoinOrCreate').value;
+				const x = (new TextEncoder().encode(s)).length;
+				if (x >= 1 && x <= 32) {
+					ookieLeaderboard.leaderboard_create(s);
+					Game.ClosePrompt();
+				}
+			`],
+			loc("Cancel"),
+		]);
+		l('leaderboardJoinOrCreate').focus();
+		l('leaderboardJoinOrCreate').select();
+	},
+	_leaderboardTabClick: function(e) {
+		const item = e.target.id.split("leaderboardTab")[1]; // this is so stupid but it's so easy
+		if (this.tabOpenTo == +item) {
+			this.tabOpenTo = null;
+			PlaySound('snd/clickOff2.mp3');
+			
+			l("leaderboardTab"+item).classList.remove("leaderboardTabSelected");
+			if (l("leaderboardTabPage")) l("leaderboardTabPage").remove();
+		} else {
+			PlaySound('snd/clickOn2.mp3');
+			this.viewLeaderboardPage(+item);
+		}
+	},
+	leaveButton: function() {
+		if (this.tabOpenTo == null) return;//?
+		PlaySound('snd/clickOn2.mp3');
+		Game.Prompt('<id LeaderboardLeaveAAAAA><h3>Are you sure you want to leave?</h3><div class="block" style="text-align:center;">if you have double-checked that you\'re leaving the correct leaderboard then type in "sayonara" (without the quotes) and hit leave</div><div class="block"><input type="text" style="text-align:center;width:100%;" id="leaderboardLeavePrompt" value=""/></div>', [
+			["leave", `
+				const s = l('leaderboardLeavePrompt').value;
+				if (s == "sayonara" || s == "さよなら" || s.startsWith("じゃ")) {
+					ookieLeaderboard.leaderboard_leave(ookieLeaderboard.tabOpenTo);
+					Game.ClosePrompt();
+				}
+			`],
+			loc("Cancel"),
+		]);
+		l('leaderboardLeavePrompt').focus();
+		l('leaderboardLeavePrompt').select();
 	},
 	viewLeaderboardPage: function(board) {
 		this.tabOpenTo = board;
 		if (l("leaderboardTabPage")) l("leaderboardTabPage").remove();
+		for (let e of document.querySelectorAll(".leaderboardTab")) {
+			if (e.id == "leaderboardTab"+board) {
+				e.classList.add("leaderboardTabSelected");
+			} else {
+				e.classList.remove("leaderboardTabSelected");
+			}
+		}
 		let page = `
-			<div id="leaderboardTabPage" style="background: black">
+			<div id="leaderboardTabPage">
 				<table>
 				<thead>
 				<tr id="leaderboardTabPageTable">
@@ -179,7 +222,7 @@ Game.registerMod("ookieLeaderboard",{
 				</thead>
 				<tbody id="leaderboardTabPageTBody">
 		`;
-		for (let v of this.boardvalues[board]) {
+		for (const v of this.boardvalues[board]) {
 			// c.name, c.total_cookies, c.cookies_per_second, (c.id = ?)
 			const style = v[3] ? ' style="outline: #00f solid 2px"' : ''; // if self...
 			page += `
@@ -194,9 +237,12 @@ Game.registerMod("ookieLeaderboard",{
 			page+`
 				</tbody>
 				</table>
+				<a class="smallFancyButton" id="leaderboardLeave" onclick="document.ookieLeaderboard.leaveButton()">leave?</a>
 			</div>
 		`);
 	},
+
+
 	init: function() {
 		const MOD = this;
 		document.ookieLeaderboard = this;//bleh
@@ -208,17 +254,38 @@ Game.registerMod("ookieLeaderboard",{
 
 		document.head.appendChild(document.createElement("style")).innerHTML = `
 			#ookieLeaderboard {
-				background:url(img/starbg.jpg);
+				//background:url(img/starbg.jpg);
+				background: url(img/shipmentBackground.png);
+				background-size: auto 100%;
 			}
 			#leaderboardTabBar {
 				display: flex;
+				justify-content: center;
+			}
+			#leaderboardTitle {
+				//margin-top: 0px;
+				font-size: 20px;
 			}
 			.leaderboardTab {
 				flex: 1;
 				padding: 1em;
 				text-align: center;
-				margin: 10px 10px 5px 10px;
+				margin: 13px 10px 5px 10px;
 				background: #000;
+				max-width: 33%;
+				font-size: 16px;
+				color: white;
+				background: url(img/darkNoise.jpg);
+				overflow: hidden;
+			}
+			.leaderboardTabSelected {
+				color: black;
+				background: url(img/mineBackground.png);
+			}
+			#leaderboardTabPage {
+				//background: url(img/shipmentBackground.png);
+				border-radius: 25px 25px 0px 0px;
+				padding-top: 1px;
 			}
 			#leaderboardTabPage > table {
 				margin: 20px 20px 20px 20px;
@@ -240,19 +307,33 @@ Game.registerMod("ookieLeaderboard",{
 			#leaderboardTabPageTBody {
 				margin-top: 22px;
 			}
-			#leaderboardTabPageTBody:before {line-height:1em; content:"\\200C"; display:block;}
+			#leaderboardTabPageTBody:before {
+				line-height: 1em;
+				content: "\\200C";
+				display: block;
+			}
 			#leaderboardTabPageTBody > td {
-				background: #fff;
+				//background: #fff;
 			}
 		`;
-
 		l('buildingsMaster').insertAdjacentHTML('afterend', `
 			<div id="ookieLeaderboard" class="row enabled">
-				<div id="leaderboardTitle">Leaderboard...</div>
+				<!--
+				<div id="leaderboardTitle" class="inset title zoneTitle">
+					c.ookie.click/er/ leaderboard
+				</div>
+				-->
+				<div id="leaderboardTabBar">
+					loading...
+				</div>
 				<div class="separatorBottom"></div>
+				<div class="productButtons" id="leaderboardProducts">
+					<div class="productButton" id="leaderboardProductJoinCreate" onclick="document.ookieLeaderboard.joinCreatePrompt()">
+						join/create a leaderboard
+					</div>
+				</div>
 			</div>
 		`);
-		this.addTabBar();
 	},
 	escapeHTML: function(s) {
 		return s.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;").replaceAll('"', "&quot;").replaceAll("'", "&apos;");
