0df2ca9f48462da90bdbd3980fc8bf37fce73ee2
rtldg <rtldg@protonmail.com>
2023-11-15 09:55:43 +0000

    More websocket WIP

--- recv.sh
+++ recv.sh
@@ -2,5 +2,7 @@
 rsync -trvzL --delete \
 	--exclude cookiepy/venv \
 	--exclude cookiepy/__pycache__ \
+	--exclude wspollpy/venv \
+	--exclude wspollpy/__pycache__ \
 	--exclude c.ookie.click/er/topeka \
 	admin@ec2:/web/* web/
--- send.sh
+++ send.sh
@@ -2,6 +2,8 @@
 rsync -trvz --delete \
 	--exclude cookiepy/venv \
 	--exclude cookiepy/__pycache__ \
+	--exclude wspollpy/venv \
+	--exclude wspollpy/__pycache__ \
 	--exclude cookiepy/disabled_registering \
 	--exclude cookiepy/disabled_leaderboard_create \
 	--exclude c.ookie.click/er/topeka \
