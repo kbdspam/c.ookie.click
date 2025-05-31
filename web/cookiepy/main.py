9a5afe707136a12bbee7e53f9b289490f9a773b3
rtldg <rtldg@protonmail.com>
2025-05-31 08:56:13 +0000

    Try to get timestamp cursors working

--- web/cookiepy/main.py
+++ web/cookiepy/main.py
@@ -1,55 +1,61 @@
-from flask import Flask,g,abort,request,jsonify
+from flask import Flask,g,abort,request,jsonify,Response
 import sqlite3
 import secrets
-import datetime
 import math
 import os.path
 import re
 import time
 
 # TODO: rate-limiting
+# TODO: setup https://docs.astral.sh/ruff/ to require type-hints...
 
 app = Flask(__name__)
 
-def get_db():
+def get_db() -> sqlite3.Connection:
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect("/data/leaderboard.db")
     return db
 
-def badnum(num):
+def badnum(num: float) -> bool:
     return num != num or math.isinf(num)
-def isOkayName(s):
+def isOkayName(s: str) -> bool:
     b = s.strip().encode("utf-8")
     if len(b) < 1 or len(b) > 31:
         return False
-    for c in b:
-        if c < 0x20:
-            return False
-    return True
+    return all(c >= 0x20 for c in b)
 
-def randcookie():
+def randcookie() -> str:
     return ''.join(secrets.choice("0123456789abcdefABCDEF") for _ in range(32))
 
-def disabled_registering():
+def disabled_registering() -> bool:
     return os.path.isfile("/data/disabled_registering")
-def disabled_leaderboard_create():
+def disabled_leaderboard_create() -> bool:
     return os.path.isfile("/data/disabled_leaderboard_create")
 
-def bad_workshop_id():
+def bad_workshop_id() -> bool:
     if time.time() > 1724017056: # Sun Aug 18 2024 21:37:36 GMT+0000
         return request.headers.get('X-My-Workshop-ID', '') != "3061304069"
     else:
         return False
 
 @app.teardown_appcontext
-def close_connection(exception):
+def close_connection(exception) -> None:
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
 
+def update_board_last_updated(cur: sqlite3.Cursor, cid_or_cookie: int | str) -> None:
+    if type(cid_or_cookie) == str:
+        res = cur.execute("SELECT id FROM clickers WHERE cookie = ?", (cid_or_cookie,)).fetchone()
+        if res == None: abort(403) # :thinking: TODO
+        cid = res[0]
+    else:
+        cid = cid_or_cookie
+    pass
+
 @app.route('/er/leaderboard/register', methods=['POST'])
-def leaderboard_register():
+def leaderboard_register() -> tuple[str, int]:
     if bad_workshop_id():
         return "db broken", 500 # shhh
     if disabled_registering():
@@ -66,7 +72,7 @@ def leaderboard_register():
     return cookie, 200
 
 @app.route('/er/leaderboard/changemyname', methods=['POST'])
-def leaderboard_changemyname():
+def leaderboard_changemyname() -> tuple[str, int]:
     if bad_workshop_id():
         return "db broken", 500 # shhh
     if disabled_registering():
@@ -78,7 +84,7 @@ def leaderboard_changemyname():
     if not isOkayName(name):
         return "name too big or too small", 400
     cur = get_db().cursor()
-
+    update_board_last_updated(cur, cookie)
     # special handling so I don't have to continue okay_name=1 'ing a person
     if re.fullmatch(r'i am #\d{3} on global', name) is not None:
         r = cur.execute("""
@@ -86,26 +92,22 @@ def leaderboard_changemyname():
             name=?,
             okay_name=(CASE okay_name WHEN -2 THEN -2 ELSE 1 END)
             WHERE cookie = ?""", (name,cookie))
-        get_db().commit()
-        if cur.rowcount > 0:
-            return "changed", 200
-        else:
-            return "no?", 400
-
-    r = cur.execute("""
-        UPDATE clickers SET
-        name=?,
-        last_updated=unixepoch(),
-        okay_name=(CASE okay_name WHEN -2 THEN -2 ELSE 0 END)
-        WHERE cookie = ?""", (name,cookie))
+    else:
+        r = cur.execute("""
+            UPDATE clickers SET
+            name=?,
+            last_updated=unixepoch(),
+            okay_name=(CASE okay_name WHEN -2 THEN -2 ELSE 0 END)
+            WHERE cookie = ?""", (name,cookie))
     get_db().commit()
     if cur.rowcount > 0:
+        # TODO: Ping me on discord to whitelist...
         return "changed", 200
     else:
         return "no?", 400
 
 @app.route('/er/leaderboard/create', methods=['POST'])
-def leaderboard_create():
+def leaderboard_create() -> tuple[str, int]:
     if bad_workshop_id():
         return "db broken", 500 # shhh
     if disabled_leaderboard_create():
@@ -123,13 +125,13 @@ def leaderboard_create():
     cid = cid[0]
     if 5 <= cur.execute("SELECT COUNT(*) FROM joinedboards WHERE clicker = ?", (cid,)).fetchone()[0]:
         return "in too many boards", 400
-    cur.execute("INSERT INTO boards(name, owner, cookie, only_owner_cookie) VALUES (?,?,?,0);", (name, cid, boardcookie))
+    cur.execute("INSERT INTO boards(name, owner, cookie, only_owner_cookie, last_updated) VALUES (?,?,?,0,unixepoch());", (name, cid, boardcookie))
     cur.execute("INSERT INTO joinedboards(clicker, board) VALUES (?, last_insert_rowid());", (cid,))
     get_db().commit()
     return "", 200
 
 @app.route('/er/leaderboard/cycleboardcookie', methods=['POST'])
-def leaderboard_cycleboardcookie():
+def leaderboard_cycleboardcookie() -> tuple[str, int]:
     if bad_workshop_id():
         return "db broken", 500 # shhh
     cookie = request.headers.get('X-My-Cookie', '')
@@ -140,7 +142,7 @@ def leaderboard_cycleboardcookie():
     clickerid = cur.execute("SELECT id FROM clickers WHERE cookie = ?", (cookie,)).fetchone()
     if clickerid == None: abort(403)
     clickerid = clickerid[0]
-    r = cur.execute("UPDATE boards SET cookie = ? WHERE owner = ? AND id = ?", (randcookie(),clickerid,boardid))
+    r = cur.execute("UPDATE boards SET cookie = ?, last_updated=unixepoch() WHERE owner = ? AND id = ?", (randcookie(),clickerid,boardid))
     get_db().commit()
     if cur.rowcount > 0:
         return "cycled", 200
@@ -148,7 +150,7 @@ def leaderboard_cycleboardcookie():
         return "no?", 400
 
 @app.route('/er/leaderboard/changeboardname', methods=['POST'])
-def leaderboard_changeboardname():
+def leaderboard_changeboardname() -> tuple[str, int]:
     if bad_workshop_id():
         return "db broken", 500 # shhh
     cookie = request.headers.get('X-My-Cookie', '')
@@ -162,7 +164,7 @@ def leaderboard_changeboardname():
     clickerid = cur.execute("SELECT id FROM clickers WHERE cookie = ?", (cookie,)).fetchone()
     if clickerid == None: abort(403)
     clickerid = clickerid[0]
-    r = cur.execute("UPDATE boards SET name = ? WHERE owner = ? AND id = ?", (name,clickerid,boardid))
+    r = cur.execute("UPDATE boards SET name = ?, last_updated=unixepoch() WHERE owner = ? AND id = ?", (name,clickerid,boardid))
     get_db().commit()
     if cur.rowcount > 0:
         return "changed", 200
@@ -170,7 +172,7 @@ def leaderboard_changeboardname():
         return "no?", 400
 
 @app.route('/er/leaderboard/kick', methods=['POST'])
-def leaderboard_kick():
+def leaderboard_kick() -> tuple[str, int]:
     if bad_workshop_id():
         return "db broken", 500 # shhh
     cookie = request.headers.get('X-My-Cookie', '')
@@ -182,34 +184,31 @@ def leaderboard_kick():
     res = cur.execute("SELECT id, can_mod FROM clickers WHERE cookie = ?", (cookie,)).fetchone()
     if res == None: abort(403)
     clickerid = res[0]
+    can_mod = res[1]
     if enemy == clickerid:
         return "no...", 400
+    can_kick = False
     if boardid == 1:
-        can_mod = res[1]
-        if can_mod >= 2:
-            cur.execute("DELETE FROM joinedboards WHERE clicker = ? AND board = ?", (enemy,boardid))
-            if cur.rowcount > 0:
-                # TODO: Maybe force the board to require a full query?
-                cur.execute("UPDATE clickers SET last_updated=unixepoch() WHERE id=?",(enemy,))
-                get_db().commit()
-                return "kicked", 200
-            else:
-                return "not in board?", 400
-        else:
-            return "no dice", 400
-    cur.execute("UPDATE boards SET cookie = ? WHERE owner = ? AND id = ?", (randcookie(),clickerid,boardid))
-    if cur.rowcount > 0:
+        can_kick = can_mod >= 2
+    else:
+        cur.execute("UPDATE boards SET cookie = ? WHERE owner = ? AND id = ?", (randcookie(),clickerid,boardid))
+        can_kick = cur.rowcount > 0
+    if can_kick:
         cur.execute("DELETE FROM joinedboards WHERE clicker = ? AND board = ?", (enemy,boardid))
         if cur.rowcount > 0:
+            cur.executescript("""
+                UPDATE clickers SET last_updated=unixepoch() WHERE id={enemy};
+                UPDATE boards SET last_updated=unixepoch() WHERE id={boardid};
+            """)
             get_db().commit()
             return "kicked", 200
         else:
             return "not in board?", 400
     else:
-        return "???", 400
+        return "no dice", 400
 
 @app.route('/er/leaderboard/updateme', methods=['POST'])
-def leaderboard_updateme():
+def leaderboard_updateme() -> tuple[str, int]:
     if bad_workshop_id():
         return "db broken", 500 # shhh
     cookie = request.headers.get('X-My-Cookie', '')
@@ -219,6 +218,7 @@ def leaderboard_updateme():
     total_cookies = float(data[0])
     cookies_per_second = float(data[1])
     cur = get_db().cursor()
+    update_board_last_updated(cur, cookie)
     if badnum(total_cookies) or total_cookies < 0 or badnum(cookies_per_second) or cookies_per_second < 0:
         cur.execute("UPDATE clickers SET cheater=1, last_updated=unixepoch() WHERE cookie = ?", (cookie,))
         get_db().commit()
@@ -232,16 +232,20 @@ def leaderboard_updateme():
         return "???", 403
 
 @app.route('/er/leaderboard/query')
-def leaderboard_query():
+def leaderboard_query() -> Response | tuple[str, int]:
     if bad_workshop_id():
         return "db broken", 500 # shhh
     cookie = request.headers.get('X-My-Cookie', '')
     if len(cookie) != 32:
         return "", 401
     try:
-        timestamp = int(request.headers.get('X-My-Timestamp', '0'))
+        timestamp = int(request.headers.get('X-My-Timestamp2', '0'))
     except:
         timestamp = 0
+    now = int(time.time())
+    max_time_period = now - (60 * 3)
+    if timestamp < 0 or timestamp > now: timestamp = 0
+    if timestamp > 0 and timestamp < max_time_period: timestamp = max_time_period
     cur = get_db().cursor()
     res = cur.execute("SELECT id, can_mod, name FROM clickers WHERE cookie = ?", (cookie,)).fetchone()
     """
@@ -272,7 +276,9 @@ def leaderboard_query():
             (c.okay_name>0 AND c.cheater=0)
         FROM joinedboards j
         JOIN clickers c ON c.id = j.clicker
+        JOIN boards b ON b.id = j.board
         WHERE j.board IN (SELECT board FROM joinedboards WHERE clicker = {cid})
+              AND ({timestamp} = 0 OR c.last_updated>={timestamp} OR b.last_updated>={timestamp})
         ORDER BY
             j.board ASC,
             (j.board=1 AND ((c.okay_name>0 AND c.cheater=0) OR c.id={cid})) DESC,
@@ -280,14 +286,13 @@ def leaderboard_query():
             c.total_cookies DESC,
             c.id ASC
     """).fetchall()
-    #AND ({timestamp} = 0 OR c.last_updated>={timestamp})
     boards = cur.execute("SELECT b.id, b.name, (CASE b.owner=? WHEN 1 THEN b.cookie ELSE '' END) FROM joinedboards j JOIN boards b ON j.board = b.id WHERE j.clicker = ? ORDER BY j.board ASC", (cid,cid,)).fetchall()
     resp = jsonify(boardinfo=boards,boardvalues=res,you=cid,can_mod=can_mod,unsafe_my_name=unsafe_my_name,timestamp=timestamp)
     resp.headers["Cache-Control"] = "no-store"
     return resp
 
 @app.route('/er/leaderboard/leave', methods=['POST'])
-def leaderboard_leave():
+def leaderboard_leave() -> tuple[str, int]:
     if bad_workshop_id():
         return "db broken", 500 # shhh
     cookie = request.headers.get('X-My-Cookie', '')
@@ -303,21 +308,29 @@ def leaderboard_leave():
     if ownerid == None: abort(403)
     ownerid = ownerid[0]
     if clickerid == ownerid:
+        cur.execute("""
+            UPDATE clickers
+            SET last_updated=unixepoch()
+            WHERE clickers.id IN (SELECT clicker FROM joinedboards WHERE board = ?)
+        """, (boardid,))
         cur.execute("DELETE FROM joinedboards WHERE board = ?;", (boardid,))
         cur.execute("DELETE FROM boards WHERE id = ?;", (boardid,))
     else:
         cur.execute("DELETE FROM joinedboards WHERE board = ? AND clicker = ?", (boardid, clickerid))
+        cur.execute("UPDATE clickers SET last_updated=unixepoch() WHERE id = ?", (clickerid,))
+        cur.execute("UPDATE boards SET last_updated=unixepoch() WHERE id = ?", (boardid,))
         # IDK if this below even works:
         #cur.execute("""
         #    DELETE FROM joinedboards
         #    LEFT JOIN clickers c ON c.cookie = ?
         #    WHERE board = ? AND clicker = c.id
         #""", (cookie, boardid))
+
     get_db().commit()
     return "left", 200
 
 @app.route('/er/leaderboard/join', methods=['POST'])
-def leaderboard_join():
+def leaderboard_join() -> tuple[str, int]:
     if bad_workshop_id():
         return "db broken", 500 # shhh
     cookie = request.headers.get('X-My-Cookie', '')
@@ -327,6 +340,8 @@ def leaderboard_join():
     if len(boardcookie) != 32:
         return "b", 401
     cur = get_db().cursor()
+    cur.execute("UPDATE clickers SET last_updated=unixepoch() WHERE cookie = ?", (cookie,))
+    cur.execute("UPDATE boards SET last_updated=unixepoch() WHERE cookie = ?", (boardcookie,))
     cur.execute("""
         INSERT INTO joinedboards (clicker, board)
         SELECT clickers.id as clicker, boards.id as board
@@ -340,12 +355,12 @@ def leaderboard_join():
     else:
         return "???", 500
 
-def make_db():
+def make_db() -> None:
     db = sqlite3.connect("/data/leaderboard.db")
     cur = db.cursor()
     cur.executescript("""
         CREATE TABLE clickers (id INTEGER PRIMARY KEY, okay_name INT NOT NULL DEFAULT 0, cheater INT NOT NULL DEFAULT 0, can_mod INT NOT NULL DEFAULT 0, last_updated INT NOT NULL DEFAULT 0, total_cookies REAL NOT NULL DEFAULT 0, cookies_per_second REAL NOT NULL DEFAULT 0, cookie TEXT NOT NULL, name TEXT NOT NULL);
-        CREATE TABLE boards (id INTEGER PRIMARY KEY, owner INT NOT NULL, only_owner_cookie INT NOT NULL, cookie TEXT NOT NULL, name TEXT NOT NULL);
+        CREATE TABLE boards (id INTEGER PRIMARY KEY, owner INT NOT NULL, only_owner_cookie INT NOT NULL, cookie TEXT NOT NULL, name TEXT NOT NULL, last_updated INT NOT NULL DEFAULT 0);
         CREATE TABLE joinedboards (clicker INT NOT NULL, board INT NOT NULL, UNIQUE(clicker, board) ON CONFLICT IGNORE);
 
         CREATE INDEX cookie_clickers ON clickers(cookie);
@@ -354,7 +369,7 @@ def make_db():
     """)
     db.commit()
 
-def migrate_db_000():
+def migrate_db_000() -> None:
     db = sqlite3.connect("/data/leaderboard.db")
     cur = db.cursor()
     cur.executescript("""
@@ -379,7 +394,7 @@ def migrate_db_000():
     db.execute("VACUUM;")
     db.commit()
 
-def migrate_db_001():
+def migrate_db_001() -> None:
     db = sqlite3.connect("/data/leaderboard.db")
     cur = db.cursor()
     cur.executescript("""
@@ -402,7 +417,7 @@ def migrate_db_001():
     db.execute("VACUUM;")
     db.commit()
 
-def migrate_db_002():
+def migrate_db_002() -> None:
     db = sqlite3.connect("/data/leaderboard.db")
     cur = db.cursor()
     cur.executescript("""
@@ -418,7 +433,25 @@ def migrate_db_002():
     db.execute("VACUUM;")
     db.commit()
 
-def insert_lots_of_fake_people(targetboard):
+def migrate_db_003() -> None:
+    db = sqlite3.connect("/data/leaderboard.db")
+    cur = db.cursor()
+    cur.executescript("""
+        DROP INDEX boards_cookie;
+
+        CREATE TABLE temp_boards (id INTEGER PRIMARY KEY, owner INT NOT NULL, only_owner_cookie INT NOT NULL, cookie TEXT NOT NULL, name TEXT NOT NULL, last_updated INT NOT NULL DEFAULT 0);
+
+        CREATE TABLE temp_clickers (id INTEGER PRIMARY KEY, okay_name INT NOT NULL DEFAULT 0, cheater INT NOT NULL DEFAULT 0, can_mod INT NOT NULL DEFAULT 0, last_updated INT NOT NULL DEFAULT 0, total_cookies REAL NOT NULL DEFAULT 0, cookies_per_second REAL NOT NULL DEFAULT 0, cookie TEXT NOT NULL, name TEXT NOT NULL);
+        INSERT INTO temp_boards (id,owner,only_owner_cookie,cookie,name) SELECT id,owner,only_owner_cookie,cookie,name FROM boards;
+        DROP TABLE boards;
+        ALTER TABLE temp_boards RENAME TO boards;
+        CREATE INDEX boards_cookie ON boards(cookie);
+    """)
+    db.commit()
+    db.execute("VACUUM;")
+    db.commit()
+
+def insert_lots_of_fake_people(targetboard: int) -> None:
     import random
     db = sqlite3.connect("/data/leaderboard.db")
     cur = db.cursor()
@@ -446,5 +479,6 @@ if __name__ == '__main__':
     #migrate_db_000()
     #migrate_db_001()
     #migrate_db_002()
+    #migrate_db_003()
     #insert_lots_of_fake_people(7)
     app.run(host='0.0.0.0', port=12345)
