8d0ea9ecccaae666d4798163ba55137c4b61a6a3
rtldg <rtldg@protonmail.com>
2025-05-31 10:01:54 +0000

    reorder boards table last_updated column

--- web/cookiepy/main.py
+++ web/cookiepy/main.py
@@ -360,7 +360,7 @@ def make_db() -> None:
     cur = db.cursor()
     cur.executescript("""
         CREATE TABLE clickers (id INTEGER PRIMARY KEY, okay_name INT NOT NULL DEFAULT 0, cheater INT NOT NULL DEFAULT 0, can_mod INT NOT NULL DEFAULT 0, last_updated INT NOT NULL DEFAULT 0, total_cookies REAL NOT NULL DEFAULT 0, cookies_per_second REAL NOT NULL DEFAULT 0, cookie TEXT NOT NULL, name TEXT NOT NULL);
-        CREATE TABLE boards (id INTEGER PRIMARY KEY, owner INT NOT NULL, only_owner_cookie INT NOT NULL, cookie TEXT NOT NULL, name TEXT NOT NULL, last_updated INT NOT NULL DEFAULT 0);
+        CREATE TABLE boards (id INTEGER PRIMARY KEY, owner INT NOT NULL, only_owner_cookie INT NOT NULL, last_updated INT NOT NULL DEFAULT 0, cookie TEXT NOT NULL, name TEXT NOT NULL);
         CREATE TABLE joinedboards (clicker INT NOT NULL, board INT NOT NULL, UNIQUE(clicker, board) ON CONFLICT IGNORE);
 
         CREATE INDEX cookie_clickers ON clickers(cookie);
@@ -439,9 +439,8 @@ def migrate_db_003() -> None:
     cur.executescript("""
         DROP INDEX boards_cookie;
 
-        CREATE TABLE temp_boards (id INTEGER PRIMARY KEY, owner INT NOT NULL, only_owner_cookie INT NOT NULL, cookie TEXT NOT NULL, name TEXT NOT NULL, last_updated INT NOT NULL DEFAULT 0);
+        CREATE TABLE temp_boards (id INTEGER PRIMARY KEY, owner INT NOT NULL, only_owner_cookie INT NOT NULL, last_updated INT NOT NULL DEFAULT 0, cookie TEXT NOT NULL, name TEXT NOT NULL);
 
-        CREATE TABLE temp_clickers (id INTEGER PRIMARY KEY, okay_name INT NOT NULL DEFAULT 0, cheater INT NOT NULL DEFAULT 0, can_mod INT NOT NULL DEFAULT 0, last_updated INT NOT NULL DEFAULT 0, total_cookies REAL NOT NULL DEFAULT 0, cookies_per_second REAL NOT NULL DEFAULT 0, cookie TEXT NOT NULL, name TEXT NOT NULL);
         INSERT INTO temp_boards (id,owner,only_owner_cookie,cookie,name) SELECT id,owner,only_owner_cookie,cookie,name FROM boards;
         DROP TABLE boards;
         ALTER TABLE temp_boards RENAME TO boards;
