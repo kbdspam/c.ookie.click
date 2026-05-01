2c655125146dc9cba14277b61ac24ab3230fb3cd
rtldg <rtldg@protonmail.com>
2026-05-01 15:26:45 +0000

    try to fix broken logic for checking if registering/creating-leaderboards

--- c-ookie-click/src/leaderboard.rs
+++ c-ookie-click/src/leaderboard.rs
@@ -106,10 +106,9 @@ fn randcookie() -> String {
 }
 
 async fn disabled_registering() -> Result<(), (StatusCode, String)> {
-	if tokio::fs::try_exists("/data/disabled_registering")
-		.await
-		.unwrap_or(false)
-	{
+	let path =
+		PathBuf::from(&std::env::var("DBPATH").unwrap_or_else(|_| "../data".to_owned())).join("disabled_registering");
+	if tokio::fs::try_exists(path).await.unwrap_or(false) {
 		Ok(())
 	} else {
 		Err((StatusCode::INTERNAL_SERVER_ERROR, "db broken".to_owned()))
@@ -117,10 +116,9 @@ async fn disabled_registering() -> Result<(), (StatusCode, String)> {
 }
 
 async fn disabled_leaderboard_create() -> Result<(), (StatusCode, String)> {
-	if tokio::fs::try_exists("/data/disabled_leaderboard_create")
-		.await
-		.unwrap_or(false)
-	{
+	let path = PathBuf::from(&std::env::var("DBPATH").unwrap_or_else(|_| "../data".to_owned()))
+		.join("disabled_leaderboard_create");
+	if tokio::fs::try_exists(path).await.unwrap_or(false) {
 		Ok(())
 	} else {
 		Err((StatusCode::INTERNAL_SERVER_ERROR, "db broken".to_owned()))
